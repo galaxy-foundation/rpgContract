ca2ed00ebfaf9c52cecf62b1eb2de523b00f4962
logan_king <77558575+loganworld@users.noreply.github.com>
2021-11-05 02:32:13 +1000

    fix:ipfs

--- scripts/upload_IPFS/upload_ipfs.js
+++ scripts/upload_IPFS/upload_ipfs.js
@@ -20,7 +20,7 @@ const upload_IFPS =async (Num)=>{
         var result = await ipfs.files.add(contents);
         var ipfsHash = basic_ipfs_url+result[0].hash;
         console.log(ipfsHash);
-        ipfsHashes[key[i]] = ipfsHash;
+        ipfsHashes[keys[i]] = ipfsHash;
 
         await delay(10000);
     } 
