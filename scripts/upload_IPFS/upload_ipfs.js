5fc0bcdb4335fedf67046900a3f3764025e8c2b5
logan_king <77558575+loganworld@users.noreply.github.com>
2021-11-05 00:55:07 +1000

    ipfs

--- scripts/upload_IPFS/upload_ipfs.js
+++ scripts/upload_IPFS/upload_ipfs.js
@@ -17,10 +17,10 @@ const upload_IFPS =async (Num)=>{
         const contents = fs.readFileSync(basic_file_path+keys[i]+".mp4");
         console.log(basic_file_path+i+".mp4");
         
-        // var result = await ipfs.files.add(contents);
-        // var ipfsHash = basic_ipfs_url+result[0].hash;
-        // console.log(ipfsHash);
-        // ipfsHashes[keys] = ipfsHash;
+        var result = await ipfs.files.add(contents);
+        var ipfsHash = basic_ipfs_url+result[0].hash;
+        console.log(ipfsHash);
+        ipfsHashes[keys] = ipfsHash;
 
         await delay(1000);
     } 
