fa0598de47a515cad0f89fb1ef1c4b6ea8a1561c
logan_king <77558575+loganworld@users.noreply.github.com>
2021-11-06 03:38:11 +1000

    update :ipfs_image

---
+++ scripts/upload_IPFS/upload_ipfs_image.js
@@ -0,0 +1,33 @@
+const ipfs = require("./ipfs_api");
+const fs = require('fs');
+const weaponInfos = require("../../resources/weaponInfos.json")
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+const upload_IFPS =async (Num)=>{
+    const basic_ipfs_url = "https://ipfs.io/ipfs/";
+
+    const basic_file_path = "./resources/images/";
+    var ipfsHashes = {};
+
+    var keys = Object.keys(weaponInfos);
+    for (var i = 0; i < keys.length; i++) {
+        const contents = fs.readFileSync(basic_file_path+keys[i]+"Moment.jpg");
+        console.log(basic_file_path+i+".mp4");
+        
+        var result = await ipfs.files.add(contents);
+        var ipfsHash = basic_ipfs_url+result[0].hash;
+        console.log(ipfsHash);
+        ipfsHashes[keys[i]] = ipfsHash;
+
+        await delay(10000);
+    } 
+    fs.writeFile("./resources/ipfshashes.json",JSON.stringify(ipfsHashes,null,4), function(err,content){
+        if (err) throw err;
+        console.log('complete');
+});
+}
+
+upload_IFPS(10)
